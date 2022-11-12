85f9077e25a33597d275a292c40ffe4edee9c334
jatin kumar <jatins9227@gmail.com>
2022-11-12 13:18:39 +0530

    added questions

--- script.js
+++ script.js
@@ -6,16 +6,23 @@ const utterances = [
     "How are you doing",
     "How are you going",
   ], //0
-  ["hi", "hey", "hello", "good morning", "good afternoon"], //1
-  ["what are you doing", "what is going on", "what is up"], //2
+  [
+    "hi",
+    "hey",
+    "hello",
+    "good morning",
+    "good afternoon",
+    "what's up",
+    "what is up",
+  ], //1
+  ["what are you doing", "what is going on"], //2
   ["how old are you"], //3
   ["who are you", "are you human", "are you bot", "are you human or bot"], //4
-  ["what's up?"], //5
-  ["good morning"], //6
-  ["good evening"], //7
-  ["good morning"], //8
-  ["good night"], //9
-  ["can you help me?"] //10
+  ["good morning"], //5
+  ["good evening"], //6
+  ["good morning"], //7
+  ["good night"], //8
+  ["can you help me?"], //9
 ];
 // Possible responses corresponding to triggers
 
@@ -38,17 +45,16 @@ const answers = [
     "I am a bot. how are you?",
     "I am a robot, but I'm a good one. Let me prove it. How can I help you?",
   ], //4
-  ["I am Great! Thanks for asking.", "Awesome like you."], //5
-  ["Good Morning! How Can I help you?"], //6
-  ["good evening! How Can I help you?"], //7
-  ["good morning! How Can I help you?"], //8
-  ["good night! How Can I help you?"], //9
-  ["Definetely! Please tell me how can i help you?"]
+  ["Good Morning! How Can I help you?"], //5
+  ["good evening! How Can I help you?"], //6
+  ["good morning! How Can I help you?"], //7
+  ["good night! How Can I help you?"], //8
+  ["Definetely! Please tell me how can i help you?"], //9
 ];
 
 // For any other user input
 
-const alternatives = ["Go on...", "Try again"];
+const alternatives = ["Go on...", "Try again", "Tell me more!"];
 
 function compare(utterancesArray, answersArray, string) {
   let item;
